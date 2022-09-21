a2f171f706f465e2695cc5fcb486452ee878820e
Wesley Wei <wesleytianqiwei@gmail.com>
2022-09-20 23:43:12 -0400

    client uppopulated resolved

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import io from 'socket.io-client';
+
+const socket =io();
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Wesley</h1>
+    </div>
+  );
+}
+
+export default App;
